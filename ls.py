559db2ee357abfb2d1c035a878911ccc61a22a88
nishoyan <91792325+nishoyan@users.noreply.github.com>
2022-10-25 22:32:17 +0530

    Update ls.py

--- ls.py
+++ ls.py
@@ -7,13 +7,13 @@ def search(arr, N, x):
     return -1
  
  
-# Driver Code
+
+ 
 if __name__ == "__main__":
     arr = [2, 3, 4, 10, 40]
     x = 10
     N = len(arr)
- 
-    # Function call
+    
     result = search(arr, N, x)
     if(result == -1):
         print("Eleent is not present in array")
